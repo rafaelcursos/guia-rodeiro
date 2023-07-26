441224d595c8dfcb9b4e19d5adf345051472a72a
Rafael Cursos <rafaelcursosdeinformatica@gmail.com>
2023-07-26 09:31:59 -0300

    novo commit realizado

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,8 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+let btnMenu = document.getElementById('btn-menu');
+
 function abreMenu() {
-    let menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
+    let nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMenu.addEventListener('click', abreMenu);
