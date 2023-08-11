ae0847100d3a7596e88a7e9a3a1871c11f0ed47a
Rafael Cursos <rafaelcursosdeinformatica@gmail.com>
2023-08-11 08:37:40 -0300

    Novo pensamento

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-let btnMenu = document.getElementById('btn-menu');
-
-function abreMenu() {
-    let nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-btnMenu.addEventListener('click', abreMenu);
